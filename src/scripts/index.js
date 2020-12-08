2c258bcb3ea25acc81b35a77b15415219d28d230
JerryD <jerry.webdev@gmail.com>
2020-12-08 15:19:11 -0500

    Completed development
    The desktop design, mobile design, alongside the functionality has been completed.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,25 @@
 import carouselData from './carouselData.js';
 
+window.addEventListener('resize', () => {
+    if (document.body.clientWidth > "375") {
+        document.querySelector("#section-1").style.backgroundImage = `url(${carouselData[0].images.desktop})`;
+        document.querySelector("#section-2__container h2").innerText = carouselData[0].title;
+        document.querySelector("#section-2__container p").innerText = carouselData[0].paragraph;
+    } else {
+        document.querySelector("#section-1").style.backgroundImage = `url(${carouselData[0].images.mobile})`;
+        document.querySelector("#section-2__container h2").innerText = carouselData[0].title;
+        document.querySelector("#section-2__container p").innerText = carouselData[0].paragraph;
+    }
+})
+
 const heroHandler = (i) => {
-    const imageSource = carouselData[i].images.desktop;
+    let imageSource;
+    if (document.body.clientWidth <= "375") {
+        imageSource = carouselData[i].images.mobile;
+    } else {
+        imageSource = carouselData[i].images.desktop;
+    }
+
     const title = carouselData[i].title;
     const paragraph = carouselData[i].paragraph;
     document.querySelector("#section-1").style.backgroundImage = `url(${imageSource})`;
@@ -11,7 +29,7 @@ const heroHandler = (i) => {
 
 let index = 0;
 
-document.getElementById("section-2__arrow_buttons").addEventListener('click', (event) => {
+document.getElementById("carousel_buttons").addEventListener('click', (event) => {
     if (event.target.id === "left-btn") {
         if (index === 0) {
             index = carouselData.length-1;
@@ -30,5 +48,21 @@ document.getElementById("section-2__arrow_buttons").addEventListener('click', (e
             index++;
             return heroHandler(index);
         }
+    }
+});
+
+const menuManager = () => {
+    if (document.getElementById("app-canvas").dataset.menu === "off-screen") {
+        document.getElementById("app-canvas").dataset.menu = "on-screen";
+        document.getElementById("menu-toggle").src = "../src/assets/images/icon-close.svg";
+    } else {
+        document.getElementById("app-canvas").dataset.menu = "off-screen";
+        document.getElementById("menu-toggle").src = "../src/assets/images/icon-hamburger.svg";
+    }
+}
+
+document.getElementById("menu-toggle").addEventListener('click', (event) => {
+    if (document.body.clientWidth <= "375") {
+        menuManager();
     }
 });
